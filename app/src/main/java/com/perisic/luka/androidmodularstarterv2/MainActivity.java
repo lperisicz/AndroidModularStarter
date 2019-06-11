8b5876508f448a8fbbb4b5a29f891873dc51776f
Luka Perisic <luka.perisic@gauss.hr>
2019-06-11 19:54:44 +0200

    initial commit

---
+++ app/src/main/java/com/perisic/luka/androidmodularstarterv2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.perisic.luka.androidmodularstarterv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
