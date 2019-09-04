62c162986b2d91a51428a0758b2c4ea085dd3478
Luka Perisic <lukaperisic97@gmail.com>
2019-09-04 11:39:50 +0200

    1.0.33 sensor added

--- app/src/main/java/com/perisic/luka/androidmodularstarterv2/MainActivity.java
+++ app/src/main/java/com/perisic/luka/androidmodularstarterv2/MainActivity.java
@@ -1,8 +1,13 @@
 package com.perisic.luka.androidmodularstarterv2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.perisic.luka.ui.SensorActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.text_view_hello_world).setOnClickListener( v ->
+                startActivityForResult(new Intent(this, SensorActivity.class), 123)
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == 123) {
+            if (data != null && data.hasExtra(SensorActivity.RESULT_KEY)) {
+                ((TextView)findViewById(R.id.text_view_hello_world)).setText(String.valueOf(data.getIntExtra(SensorActivity.RESULT_KEY, 0)));
+            }
+        }
     }
 }
