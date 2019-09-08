255d24d0a79fdf57f64172482697bca06a63bc8a
Luka Perisic <lukaperisic97@gmail.com>
2019-09-08 18:47:51 +0200

    1.0.35

--- app/src/main/java/com/perisic/luka/androidmodularstarterv2/MainActivity.java
+++ app/src/main/java/com/perisic/luka/androidmodularstarterv2/MainActivity.java
@@ -1,13 +1,13 @@
 package com.perisic.luka.androidmodularstarterv2;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.perisic.luka.ui.SensorActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import hr.gauss.camera.CameraActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,17 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.text_view_hello_world).setOnClickListener( v ->
-                startActivityForResult(new Intent(this, SensorActivity.class), 123)
+        findViewById(R.id.text_view_hello_world).setOnClickListener(v ->
+                startActivityForResult(new Intent(this, CameraActivity.class), 123)
+//                        startActivity(new Intent(this, CameraActivity.class))
         );
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK && requestCode == 123) {
-            if (data != null && data.hasExtra(SensorActivity.RESULT_KEY)) {
-                ((TextView)findViewById(R.id.text_view_hello_world)).setText(String.valueOf(data.getIntExtra(SensorActivity.RESULT_KEY, 0)));
+        if (resultCode == RESULT_OK && requestCode == 123) {
+            if (data != null && data.hasExtra(CameraActivity.RESULT_KEY)) {
+                ((TextView)findViewById(R.id.text_view_hello_world)).setText(data.getStringExtra(CameraActivity.RESULT_KEY));
             }
         }
     }
